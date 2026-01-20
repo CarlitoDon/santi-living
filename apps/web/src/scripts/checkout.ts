873d5a18f839f0ecd2be566e28efe3f6b007f8d1
Khusnudhoni Hendra M <66800518+CarlitoDon@users.noreply.github.com>
2026-01-20 19:04:45 +0700

    fix(web): Add missing onClose type definition in SnapEmbedOptions

--- apps/web/src/scripts/checkout.ts
+++ apps/web/src/scripts/checkout.ts
@@ -37,6 +37,7 @@ interface SnapEmbedOptions {
   onSuccess: (result: Record<string, unknown>) => void;
   onPending: (result: Record<string, unknown>) => void;
   onError: (result: Record<string, unknown>) => void;
+  onClose?: () => void;
 }
 
 interface CheckoutState {
