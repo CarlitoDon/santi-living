74c79c7af0f2f95025bcbb028888a18f2be771bd
Khusnudhoni Hendra M <66800518+CarlitoDon@users.noreply.github.com>
2026-04-17 17:48:48 +0700

    chore(eslint): allow default project parsing for mjs configs to fix IDE error

--- apps/proxy/eslint.config.mjs
+++ apps/proxy/eslint.config.mjs
@@ -7,7 +7,9 @@ export default tseslint.config(
   {
     languageOptions: {
       parserOptions: {
-        projectService: true,
+        projectService: {
+          allowDefaultProject: ["*.js", "*.mjs"],
+        },
         tsconfigRootDir: import.meta.dirname,
       },
     },
