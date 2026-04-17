ba420046020064341a2a00a7f618c5e21ec7c7b8
Khusnudhoni Hendra M <66800518+CarlitoDon@users.noreply.github.com>
2026-04-17 15:51:22 +0700

    chore: remove Astro app, migrate domain to Next.js, clean up monorepo

--- apps/web/src/components/calculator/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Calculator component exports
- */
-
-export { Calculator } from "./Calculator";
-export { useCalculatorState } from "./useCalculatorState";
-export type { CalculatorState, CartItem, CustomerData, Product } from "./types";
